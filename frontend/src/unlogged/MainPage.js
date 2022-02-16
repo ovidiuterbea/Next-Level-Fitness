4c9d7eeaacd0b23cb3f4e3fbdb75fbc3b78429f8
Terbea Ovidiu <ovidiuminecraft1234@gmail.com>
2022-02-16 12:12:55 +0200

    added video to the main page

--- frontend/src/unlogged/MainPage.js
+++ frontend/src/unlogged/MainPage.js
@@ -2,17 +2,18 @@ import React from "react";
 import "./style.css";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import ReactPlayer from "react-player";
 
 const MainPage = () => {
   return (
     <React.Fragment>
       <header className="hero">
-        <div class="content">
+        <div className="content">
           <h1>Train to the next level</h1>
           <p>Be the best version of yourself</p>
         </div>
       </header>
-      <div className="container-principal">
+      <div className="container-principal__black">
         <div className="container-secundar1">
           <h3>
             Atinge-ti scopurile cu abonamentul{" "}
@@ -24,12 +25,33 @@ const MainPage = () => {
           </h3>
         </div>
         <div className="container-secundar2">
-          <h3 className="abonament-pret">Doar 150 de lei/luna</h3>
+          <h3 className="abonament-pret">De la 150 de lei/luna</h3>
           <Link to="/tarife">
-            <Button id="muibtn" style={{ color: "#f3f3f3" }}>
-              Vezi alte abonamente
-            </Button>
+            <Button id="muibtn">Vezi toate abonamentele</Button>
           </Link>
+        </div>
+      </div>
+      <div className="container-principal__white">
+        <div className="container-secundar1">
+          <h2>De ce sa te alaturi comunitatii noastre?</h2>
+          <h4>
+            Aparatura de ultima generatie, buna si niste oameni care te vor
+            ajuta sa integrezi foarte usor
+          </h4>
+          <p>
+            Intelegem faptul ca este greu la inceput, dar cu timpul, fitness-ul
+            devine un stil de viata care te ajuta sa evoluezi
+          </p>
+        </div>
+        <div className="container-secundar2">
+          <h3>Antreneaza-te dupa stilul tau</h3>
+          <ReactPlayer
+            url="mainpage-video.mp4"
+            playing={true}
+            muted={true}
+            controls={false}
+            loop={true}
+          />
         </div>
       </div>
     </React.Fragment>
