aae87604593c87686819b1af1f564e14bc8b1ddd
ovidiuterbea <ovidiuminecraft1234@gmail.com>
2022-03-06 12:47:58 +0200

    imported react scheduler package

--- frontend/src/gym-classes/pages/Classes.js
+++ frontend/src/gym-classes/pages/Classes.js
@@ -1,4 +1,11 @@
 import React from "react";
+import { Paper } from "@mui/material";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  WeekView,
+  Appointments,
+} from "@devexpress/dx-react-scheduler-material-ui";
 
 const Classes = (props) => {
   return <React.Fragment>Aceasta este pagina de Classes</React.Fragment>;
