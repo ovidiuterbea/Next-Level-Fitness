dada95eab962febc5248e2a1f3be8592f57b9309
ovidiuterbea <ovidiuminecraft1234@gmail.com>
2022-03-06 17:46:53 +0200

    added Delete on the classes

--- frontend/src/gym-classes/pages/Classes.js
+++ frontend/src/gym-classes/pages/Classes.js
@@ -10,7 +10,11 @@ import {
   TodayButton,
   AppointmentTooltip,
   AppointmentForm,
+  DayView,
 } from "@devexpress/dx-react-scheduler-material-ui";
+import { styled } from "@mui/material/styles";
+import { IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const Classes = (props) => {
   const [loadedClassesFetch, setLoadedClassesFetch] = useState();
@@ -19,28 +23,57 @@ const Classes = (props) => {
     const getClasses = async () => {
       const response = await fetch(`http://localhost:8080/api/classes/`);
       const data = await response.json();
-      setLoadedClassesFetch(data);
+      for (const gymClass of data.classes) {
+        gymClass.startDate = new Date(gymClass.startDate);
+        gymClass.endDate = new Date(gymClass.endDate);
+      }
+      setLoadedClassesFetch(data.classes);
     };
     getClasses();
   }, []);
+
+  // const deleteClassHandler = async () => {
+  //   await fetch(`http://localhost:8080/api/hiring/${hiringRequestId}`, {
+  //     method: "DELETE",
+  //   });
+  // };
+
+  const Header = ({ children, appointmentData, ...restProps }) => (
+    <AppointmentTooltip.Header {...restProps} appointmentData={appointmentData}>
+      <IconButton
+        onClick={async () => {
+          await fetch(
+            `http://localhost:8080/api/classes/${appointmentData.id}`,
+            {
+              method: "DELETE",
+            }
+          );
+          setLoadedClassesFetch(
+            loadedClassesFetch.filter(
+              (gymClass) => gymClass.id !== appointmentData.id
+            )
+          );
+        }}
+        size='large'
+      >
+        <DeleteIcon />
+      </IconButton>
+    </AppointmentTooltip.Header>
+  );
 
   return (
     <React.Fragment>
       {loadedClassesFetch && (
         <Paper>
-          <Scheduler height='auto' data={loadedClassesFetch.classes}>
-            <EditingState allow />
+          <Scheduler height='auto' data={loadedClassesFetch}>
             <ViewState />
+            <EditingState />
             <WeekView startDayHour={9} endDayHour={21} />
             <Toolbar />
             <DateNavigator />
             <TodayButton />
             <Appointments />
-            <AppointmentTooltip
-              showCloseButton
-              showOpenButton
-              showDeleteButton
-            />
+            <AppointmentTooltip showCloseButton headerComponent={Header} />
             <AppointmentForm />
           </Scheduler>
         </Paper>
