553b5d3599e495fcc7b024e374b3a10a1cc74038
ovidiuterbea <ovidiuminecraft1234@gmail.com>
2022-03-17 18:03:04 +0200

    refactored payment filtering

--- frontend/src/users/components/UserList.js
+++ frontend/src/users/components/UserList.js
@@ -1,45 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import UserItem from "./UserItem";
 import "./UserList.css";
 
 const UserList = (props) => {
-  const [showPaymentList, setShowPaymentList] = useState(false);
-
-  for (let client of props.items.clients) {
-    if (client.mustPay === true) {
-      setShowPaymentList(true);
-    }
-  }
-  if (showPaymentList === false) {
-    return (
-      <div className='subscription-detail'>
-        <h2>No Payments from clients found!</h2>
-      </div>
-    );
-  }
-
-  let mustPayCondition;
   return (
     <ul className='card-user'>
-      {props.items.clients.map((client) => {
-        if (client.mustPay === true) {
-          mustPayCondition = true;
-        }
-        return (
-          <UserItem
-            key={client.id}
-            id={client.id}
-            name={client.name}
-            surname={client.surname}
-            mustPay={client.mustPay}
-            image={client.image}
-            birthdate={client.birthdate}
-            address={client.address}
-            subscription={client.subscription}
-            mustPayCondition={mustPayCondition}
-          />
-        );
-      })}
+      {props.items.clients
+        .filter((client) => client.mustPay === true)
+        .map((client) => {
+          return (
+            <UserItem
+              key={client.id}
+              id={client.id}
+              name={client.name}
+              surname={client.surname}
+              mustPay={client.mustPay}
+              image={client.image}
+              birthdate={client.birthdate}
+              address={client.address}
+              subscription={client.subscription}
+            />
+          );
+        })}
     </ul>
   );
 };
