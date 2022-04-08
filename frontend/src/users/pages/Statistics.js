a2113660f59f24ecac8fe48ae12294990487ceb5
ovidiuterbea <ovidiuminecraft1234@gmail.com>
2022-04-08 15:17:46 +0300

    fetched data to statistics page

--- frontend/src/users/pages/Statistics.js
+++ frontend/src/users/pages/Statistics.js
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Statistics = (props) => {
+  const [loadedTrainersFetch, setLoadedTrainersFetch] = useState();
+  const [loadedClassesFetch, setLoadedClassesFetch] = useState();
+  const [loadedClientsFetch, setLoadedClientsFetch] = useState();
+
+  useEffect(() => {
+    const fetchTrainers = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/api/trainers/`);
+        const data = await response.json();
+        setLoadedTrainersFetch(data);
+      } catch (err) {}
+    };
+    fetchTrainers();
+  }, []);
+
+  useEffect(() => {
+    const fetchClasses = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/api/classes/`);
+        const data = await response.json();
+        setLoadedClassesFetch(data.classes);
+      } catch (err) {}
+    };
+    fetchClasses();
+  }, []);
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/api/clients/`);
+        const data = await response.json();
+        setLoadedClientsFetch(data.clients);
+      } catch (err) {}
+    };
+    fetchClients();
+  }, []);
+
+  if (loadedTrainersFetch && loadedClassesFetch && loadedClientsFetch) {
+    console.log(loadedTrainersFetch);
+    console.log(loadedClassesFetch);
+    console.log(loadedClientsFetch);
+  }
+
   return (
     <div
       className='normal-card'
