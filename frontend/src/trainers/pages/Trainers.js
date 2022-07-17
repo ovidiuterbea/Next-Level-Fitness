2622b19a2f0c9ecda4549f449aa926e0d6da7bda
ovidiuterbea <ovidiuminecraft1234@gmail.com>
2022-07-17 20:12:09 +0300

    removed unused components

--- frontend/src/trainers/pages/Trainers.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Trainers = (props) => {
-  return <React.Fragment>Aceasta este pagina de Trainers</React.Fragment>;
-};
-
-export default Trainers;
--- frontend/src/users/pages/UserDetails.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserDetails = (props) => {
-  return <React.Fragment>Aceasta este pagina de UserDetails</React.Fragment>;
-};
-
-export default UserDetails;
